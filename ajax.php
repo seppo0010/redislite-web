12cdea7a4bd04ef7d43861ac912de894c8d61222
Sebastian Waisbrot <seppo0010@gmail.com>
2011-08-14 21:03:46 -0300

    Remove test code

--- ajax.php
+++ ajax.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
 $id = session_id();
-$id = "test";
 @mkdir('databases/' . substr($id, 0, 2));
-$_POST['command']="GET b";
-echo shell_exec('FAKETTY=1 ./redislite-cli -f databases/' . substr($id, 0, 2) . '/' . substr($id, 2) . ' <<<' . escapeshellarg($_POST['command']));
+echo shell_exec('FAKETTY=1 ./redislite/src/redislite-cli -f databases/' . substr($id, 0, 2) . '/' . substr($id, 2) . ' <<<' . escapeshellarg($_POST['command']));
